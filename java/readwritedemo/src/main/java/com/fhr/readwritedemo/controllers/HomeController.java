97a78d9119b2261d2408c53e778861ff3b60ebd8
FanHuaRan <834171100@qq.com>
2017-07-28 23:02:29 +0800

    2017/07/28 fhr

---
+++ java/readwritedemo/src/main/java/com/fhr/readwritedemo/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.fhr.readwritedemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+
+}
